cf37ab058c638584843926b398af561ce36af11b
Marco Kellershoff <marco@kellershoff.net>
2024-06-29 12:36:14 +0200

    fix(release): missing git is dirty function

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,13 @@ set_version() {
   ./scripts/set-version.sh
 }
 
+check_git_dirty() {
+  if [[ -n $(git status -s) ]]; then
+    echo "Working directory is dirty. Please commit or stash your changes before releasing."
+    exit 1
+  fi
+}
+
 do_gh_release() {
   echo "Creating new release $GH_TAG"
   gh release create --generate-notes "$GH_TAG"
