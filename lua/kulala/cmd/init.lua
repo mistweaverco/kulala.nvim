bcf4ff6b5c16611b9d8cecefac3c67bb4754af1f
Coltor Leichtman <coltorleichtman@gmail.com>
2024-07-09 20:32:45 -0500

    make cmd non-blocking

--- lua/kulala/cmd/init.lua
+++ lua/kulala/cmd/init.lua
@@ -5,18 +5,16 @@ local FS = require("kulala.utils.fs")
 
 local M = {}
 
-local function exec_cmd(cmd)
-  return vim.system(cmd, { text = true }):wait().stdout
-end
-
 -- runs the cmd and maybe formats the result
 M.run = function(result)
-  exec_cmd(result.cmd)
-  if result.ft then
-    local body = FS.read_file(GLOBALS.BODY_FILE)
-    FS.write_file(GLOBALS.BODY_FILE, FORMATTER.format(result.ft, body))
-  end
+	vim.fn.jobstart(result.cmd, {
+		on_stdout = function()
+			if result.ft then
+				local body = FS.read_file(GLOBALS.BODY_FILE)
+				FS.write_file(GLOBALS.BODY_FILE, FORMATTER.format(result.ft, body))
+			end
+		end,
+	})
 end
 
 return M
-
