c9da4db8d59608f229ddaf46c346a293dae2a48d
Marco Kellershoff <marco@kellershoff.net>
2024-08-06 00:02:27 +0200

    chore(version): bump

--- lua/kulala/globals/init.lua
+++ lua/kulala/globals/init.lua
@@ -2,7 +2,7 @@ local FS = require("kulala.utils.fs")
 
 local M = {}
 
-M.VERSION = "2.9.0"
+M.VERSION = "2.10.0"
 M.UI_ID = "kulala://ui"
 M.SCRATCHPAD_ID = "kulala://scratchpad"
 M.HEADERS_FILE = FS.get_plugin_tmp_dir() .. "/headers.txt"
