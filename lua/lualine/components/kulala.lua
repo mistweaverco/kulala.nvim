601bf6f4ba7ffb34d3b3480982d8123a9c89bae4
Marco Kellershoff <1384938+gorillamoe@users.noreply.github.com>
2024-08-15 00:00:07 +0200

    fix(lualine): default env

--- lua/lualine/components/kulala.lua
+++ lua/lualine/components/kulala.lua
@@ -23,7 +23,7 @@ end
 function M.update_status()
   local current_filetype = vim.bo.filetype
   if current_filetype == "http" then
-    return vim.g.kulala_selected_env
+    return vim.g.kulala_selected_env or CONFIG.get().default_env
   end
   return ""
 end
