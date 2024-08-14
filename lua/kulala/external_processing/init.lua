03ad4ee43f2aa3466aa9503d411db37b6e5da0b6
Marco Kellershoff <marco@kellershoff.net>
2024-08-14 18:09:11 +0200

    fix ext-processing + add jwt example

--- lua/kulala/external_processing/init.lua
+++ lua/kulala/external_processing/init.lua
@@ -1,26 +1,47 @@
 local FS = require("kulala.utils.fs")
+local ShellUtils = require("kulala.cmd.shell_utils")
 local DB = require("kulala.db")
+local Logger = require("kulala.logger")
 
 local M = {}
 
 M.env_stdin_cmd = function(cmdstring, contents)
   local cmd = {}
-  local splitted = vim.split(cmdstring, " ")
-  local env_name = splitted[1]
-  local cmd_exists = FS.command_exists(splitted[2])
-  if not cmd_exists then
-    vim.notify("env_stdin_cmd --> Command not found: " .. cmd[2] .. ".", vim.log.levels.ERROR)
+  if ShellUtils.has_sh() then
+    -- Use sh on Unix-like systems
+    table.insert(cmd, "sh")
+    table.insert(cmd, "-c")
+  elseif ShellUtils.has_zsh() then
+    -- Use zsh on macOS
+    table.insert(cmd, "zsh")
+    table.insert(cmd, "-c")
+  elseif ShellUtils.has_powershell() then
+    -- Use PowerShell on Windows
+    table.insert(cmd, "powershell")
+    table.insert(cmd, "-Command")
+  else
+    Logger.error("env_stdin_cmd --> Shell not found: powershell, sh, or zsh.")
     return
   end
-  table.remove(splitted, 1)
-  for _, token in ipairs(splitted) do
-    table.insert(cmd, token)
+
+  -- Extract environment variable name (first token)
+  local env_name, cmd_string = cmdstring:match("^(%S+)(.*)$")
+  if not env_name then
+    Logger.error("env_stdin_cmd --> Malformed metatag")
+    return
   end
+
+  -- Append the command string to the command table
+  table.insert(cmd, cmd_string)
+
+  -- Execute the command with the provided contents as stdin
   local res = vim.system(cmd, { stdin = contents, text = true }):wait().stdout
-  if res == nil then
-    vim.notify("env_stdin_cmd --> Command failed: " .. cmd[2] .. ".", vim.log.levels.ERROR)
+
+  if not res then
+    Logger.error("env_stdin_cmd --> Command failed: " .. cmdstring .. ".")
     return
   else
+    -- Save the result to the environment variable
     DB.data.env[env_name] = res
   end
 end
