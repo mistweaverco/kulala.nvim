efd90459d9891aeac32ab027ade65c23f02dca41
Michal Vyskocil <michal.vyskocil@gmail.com>
2024-09-05 09:10:15 +0200

    read formatters from config, print kulala version

--- lua/kulala/health.lua
+++ lua/kulala/health.lua
@@ -1,23 +1,30 @@
+local CONFIG = require("kulala.config")
+local GLOBALS = require("kulala.globals")
+local FS = require("kulala.utils.fs")
+
 local health = vim.health
 local start = health.start
 local ok = health.ok
+local info = health.info
+local warn = health.warn
 local error = health.error
-local is_win = vim.api.nvim_call_function("has", { "win32" }) == 1
 
 local M = {}
 
 M.check = function()
-  start("Checking external dependencies")
-  local deps = { "curl", "jq", "xmllint" }
-  for _, dep in pairs(deps) do
-    if is_win then
-      dep = dep .. ".exe"
-    end
-    local found = (vim.fn.executable(dep) == 1)
-    if found then
-      ok(string.format("Found %s", dep))
+  info("{kulala.nvim} version " .. GLOBALS.VERSION)
+  if FS.command_exists("curl") then
+    ok("{curl} found")
+  else
+    error("{curl} not found")
+  end
+
+  start("Checking formatters")
+  for type, config in pairs(CONFIG.get().contenttypes) do
+    if not config.formatter then
+      warn(string.format("{%s} formatter not found", type))
     else
-      error(string.format("Missing %s", dep))
+      ok(string.format("{%s} formatter: %s", type, table.concat(config.formatter, " ")))
     end
   end
 end
