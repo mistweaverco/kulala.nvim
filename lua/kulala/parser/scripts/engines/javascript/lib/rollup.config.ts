df1076ef3428bdaa91ae0c25ca6894907c7da438
Marco Kellershoff <marco@kellershoff.net>
2024-10-08 18:03:38 +0200

    feat(scripts): add request to post_request script

--- lua/kulala/parser/scripts/engines/javascript/lib/rollup.config.ts
+++ lua/kulala/parser/scripts/engines/javascript/lib/rollup.config.ts
@@ -36,6 +36,7 @@ export default [
           reserved: [
             'client',
             'response',
+            'request',
           ],
         },
       }),
--- lua/kulala/parser/scripts/engines/javascript/lib/src/post_request.ts
+++ lua/kulala/parser/scripts/engines/javascript/lib/src/post_request.ts
@@ -1,6 +1,7 @@
 import { Client } from './lib/Client';
 import { Response } from './lib/PostRequestResponse';
+import { Request } from './lib/PostRequest';
 
 export const client = Client;
 export const response = Response;
-
+export const request = Request;
