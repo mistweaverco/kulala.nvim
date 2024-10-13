e44d33b228bfe2c1a966f634b73ec45969fcbefd
Marco Kellershoff <marco@kellershoff.net>
2024-10-13 23:19:06 +0200

    debug(ci): windows caching

--- scripts/install-ci-test-requirements.ps1
+++ scripts/install-ci-test-requirements.ps1
@@ -1,6 +1,6 @@
 $Env:KULALA_ROOT_DIR = (Get-Location).Path
 
-if (!(Test-Path .tests)) {
+if ($Env:GH_CACHE_HIT -eq $null) {
   mkdir .tests
 }
 
@@ -8,19 +8,22 @@ cd .tests
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-if (!(Test-Path ~/scoop)) {
+if ($Env:GH_CACHE_HIT -eq $null) {
   Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-
   scoop install main/git
   scoop install main/neovim@0.10.2
+} else {
+  $Env:PATH = "$Env:USERPROFILE\scoop\shims;$Env:USERPROFILE\scoop\apps\git\current\cmd;$Env:USERPROFILE\scoop\apps\neovim\current\bin;$Env:PATH"
 }
 
-if (!(Test-Path luajit-for-win64-0.0.2)) {
+if ($Env:GH_CACHE_HIT -eq $null) {
   Invoke-RestMethod -Uri https://github.com/mistweaverco/luajit-for-win64/archive/refs/tags/v0.0.2.zip -outfile luajit.zip
   7z x luajit.zip
   RM luajit.zip
   cd luajit-for-win64-0.0.2
   .\luajit-for-win64.cmd
+} else {
+  cd luajit-for-win64-0.0.2
 }
 
 $Env:KULALA_LUA_DIR = (Get-Location).Path
@@ -29,14 +32,9 @@ $Env:PATH = "$Env:KULALA_LUA_DIR\tools\cmd;$Env:KULALA_LUA_DIR\tools\PortableGit
 $Env:LUA_PATH = "$Env:KULALA_LUA_DIR\lua\?.lua;$Env:KULALA_LUA_DIR\lua\?\init.lua;$Env:APPDATA\luarocks\share\lua\5.1\?.lua;$Env:APPDATA\luarocks\share\lua\5.1\?\init.lua;$Env:LUA_PATH"
 $Env:LUA_CPATH = "$Env:APPDATA\luarocks;$Env:APPDATA\luarocks\lib\lua\5.1\?.dll;$Env:LUA_CPATH"
 
-Write-Host "Path"
-Write-Host $Env:PATH.Split(';')
-Write-Host "LUA_PATH"
-Write-Host $Env:LUA_PATH.Split(';')
-Write-Host "LUA_CPATH"
-Write-Host $Env:LUA_CPATH.Split(';')
-
-luarocks install --lua-version 5.1 busted
+if ($Env:GH_CACHE_HIT -eq $null) {
+  luarocks install --lua-version 5.1 busted
+}
 
 # Persist the Environment Variables
 "PATH=$Env:Path" >> $Env:GITHUB_ENV
