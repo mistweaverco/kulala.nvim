d36360c47c8f89bbe3d3c1c37bee62e23fae8f9d
Marco Kellershoff <marco@kellershoff.net>
2024-10-14 03:05:17 +0200

    refactor(ci): tests

--- tests/util/fs_spec.lua
+++ tests/util/fs_spec.lua
@@ -14,39 +14,42 @@ describe("kulala.utils.fs", function()
     snapshot:revert()
   end)
 
-  describe("join_paths", function()
-    if Fs.os == "windows" then
-      it("joins mixed on windows", function()
-        local expected = "C:\\a\\b\\c"
-        local actual = Fs.join_paths("C:\\a", "b", "c")
-        assert.are.same(expected, actual)
-      end)
-      it("joins no-mixed on windows", function()
-        local expected = "C:\\a\\b\\c"
-        local actual = Fs.join_paths("C:\\a", "b", "c")
-        assert.are.same(expected, actual)
-      end)
-      it("fixes ps on windows", function()
-        local expected = "C:\\a\\user\\bin\\blah\\blubb"
-        local actual = Fs.join_paths("C:\\a", "user/bin", "blah/blubb")
-        assert.are.same(expected, actual)
-      end)
-    else
-      it("joins mixed on unix", function()
-        local expected = "/a/b/c"
-        local actual = Fs.join_paths("/a", "b", "c")
-        assert.are.same(expected, actual)
-      end)
-      it("joins no-mixed on unix", function()
-        local expected = "/a/b/c"
-        local actual = Fs.join_paths("/a", "b", "c")
-        assert.are.same(expected, actual)
-      end)
-      it("joins more mixed on unix", function()
-        local expected = "/a/user/bin/blah/blubb"
-        local actual = Fs.join_paths("/a", "user/bin", "blah/blubb")
-        assert.are.same(expected, actual)
-      end)
-    end
+  describe("join_paths on windows", function()
+    Fs.os = "windows"
+    Fs.ps = "\\"
+    it("joins mixed on windows", function()
+      local expected = "C:\\a\\b\\c"
+      local actual = Fs.join_paths("C:\\a", "b", "c")
+      assert.are.same(expected, actual)
+    end)
+    it("joins no-mixed on windows", function()
+      local expected = "C:\\a\\b\\c"
+      local actual = Fs.join_paths("C:\\a", "b", "c")
+      assert.are.same(expected, actual)
+    end)
+    it("fixes ps on windows", function()
+      local expected = "C:\\a\\user\\bin\\blah\\blubb"
+      local actual = Fs.join_paths("C:\\a", "user/bin", "blah/blubb")
+      assert.are.same(expected, actual)
+    end)
+  end)
+  describe("join_paths on linux", function()
+    Fs.os = "unix"
+    Fs.ps = "/"
+    it("joins mixed on unix", function()
+      local expected = "/a/b/c"
+      local actual = Fs.join_paths("/a", "b", "c")
+      assert.are.same(expected, actual)
+    end)
+    it("joins no-mixed on unix", function()
+      local expected = "/a/b/c"
+      local actual = Fs.join_paths("/a", "b", "c")
+      assert.are.same(expected, actual)
+    end)
+    it("joins more mixed on unix", function()
+      local expected = "/a/user/bin/blah/blubb"
+      local actual = Fs.join_paths("/a", "user/bin", "blah/blubb")
+      assert.are.same(expected, actual)
+    end)
   end)
 end)
