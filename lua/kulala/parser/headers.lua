199bff37b89ccd0f1054422670f9fda9baa1428d
Michal Vyskocil <michal.vyskocil@gmail.com>
2024-10-24 18:24:21 +0200

    implement the PR review
     * has have an optional parameter value, if not nil, then name/value
       pair is searched, case insensitive
     * has_exact does a case_sensitive search of name and value

--- lua/kulala/parser/headers.lua
+++ lua/kulala/parser/headers.lua
@@ -1,8 +1,8 @@
 --- This class is used to manage the HTTP headers
---- HTTP header has a case-insensitive name - so class store it in a lower case
---- and can have a multiple values - which are stored in a table
+--- HTTP header has a case-insensitive name, so for set and lookup purposes those are stored in a lower-case
+--- The original case is saved and returned via flatpairs method.
 ---@class HTTPHeaders
----@field _headers table<string, string[]> - internal table storing the HTTP headers
+---@field _headers table<string, table> - internal table storing the HTTP headers
 local HTTPHeaders = {}
 
 --- new creates a new instance of HTTPHeaders
@@ -11,57 +11,98 @@ function HTTPHeaders.new()
   return setmetatable({ _headers = {} }, HTTPHeaders)
 end
 
---- has checks if a given name exists
+--- has checks if a given name exists and optional value, case insensitive
+--- eg. headers.has("content-type") will match Content-Type too
+--- eg. headers.has("content-type", "Text/plain") will match Content-Type, "text/plain"
 ---@param self HTTPHeaders
----@param key string
+---@param name string
+---@param value string|nil
 ---@return boolean
-function HTTPHeaders.has(self, key)
-  return self._headers[key:lower()] ~= nil
-end
-
---- has checks if a given name exists and contains a specific value
----@param self HTTPHeaders
----@param key string
----@param value string
----@return boolean
-function HTTPHeaders.has_value(self, key, value)
-  if not self:has(key) then
-    return false
+function HTTPHeaders.has(self, name, value)
+  local key = name:lower()
+  local exists = self._headers[key] ~= nil
+  if value == nil then
+    return exists
   end
-  for _, v in ipairs(self._headers[key:lower()]) do
-    if v == value then
+
+  for _, e in ipairs(self._headers[key]) do
+    if e.value:lower() == value:lower() then
       return true
     end
   end
   return false
 end
 
+--- has_exact checks if a given name exists, case sensitive
+--- eg. headers.has("X-REQUEST-TYPE") won't match x-request-type
+--- eg. headers.has("X-REQUEST-TYPE", "graphql") won't match x-request-type GRAPHQL
 ---@param self HTTPHeaders
----@param key string
+---@param name string
+---@param value string|nil
+---@return boolean
+function HTTPHeaders.has_exact(self, name, value)
+  local key = name:lower()
+  if value == nil then
+    for _, e in ipairs(self._headers[key]) do
+      if e.name == name then
+        return true
+      end
+      return false
+    end
+  end
+
+  for _, e in ipairs(self._headers[key]) do
+    if e.name == name and e.value == value then
+      return true
+    end
+  end
+  return false
+end
+
+--- return a first value of a given name, case insensitive
+--- eg. headers["content-type"]
+--- use values() to get all values
+---@param self HTTPHeaders
+---@param name string
 ---@return any
-function HTTPHeaders.__index(self, key)
-  return rawget(getmetatable(self), key) or rawget(self._headers, key:lower())
+function HTTPHeaders.__index(self, name)
+  local key = name:lower()
+  local ret = rawget(getmetatable(self), key)
+  if ret ~= nil then
+    return ret
+  end
+  ret = rawget(self._headers, key)
+  if ret ~= nil then
+    local values = {}
+    for _, e in ipairs(ret) do
+      values[#values + 1] = e.value
+    end
+    return values
+  end
+  return nil
 end
 
+--- append a new value to the header. The name is case insensitive and original casing is preserved
 ---@param self HTTPHeaders
----@param key string
+---@param name string
 ---@param value string
-function HTTPHeaders.__newindex(self, key, value)
-  local values = rawget(self._headers, key:lower()) or {}
-  values[#values + 1] = value
-  rawset(self._headers, key:lower(), values)
+function HTTPHeaders.__newindex(self, name, value)
+  local key = name:lower()
+  local values = rawget(self._headers, key) or {}
+  values[#values + 1] = { name = name, value = value }
+  rawset(self._headers, key, values)
 end
 
---- flatpairs returns a pair of a name, single-value, where names are repeated
+--- flatpairs returns a pair of a name, single-value for all values. The original header name is returned
 ---@param self HTTPHeaders
 ---@return function
 ---@return HTTPHeaders
 ---@return nil
 function HTTPHeaders.flatpairs(self)
   local co = coroutine.create(function()
-    for key, values in pairs(self._headers) do
-      for _, value in ipairs(values) do
-        coroutine.yield(key, value)
+    for _, values in pairs(self._headers) do
+      for _, e in ipairs(values) do
+        coroutine.yield(e.name, e.value)
       end
     end
   end)
@@ -69,15 +110,15 @@ function HTTPHeaders.flatpairs(self)
   ---@return integer|nil
   ---@return string[]|nil
   local next = function(_, _)
-    local key, value
+    local name, value
     repeat
-      _, key, value = coroutine.resume(co)
+      _, name, value = coroutine.resume(co)
       idx = idx + 1
-      if key == nil then
+      if name == nil then
         return nil, nil
       end
-      return idx, { key, value }
-    until key ~= nil
+      return idx, { name, value }
+    until name ~= nil
   end
   return next, self, nil
 end
--- tests/parser/headers_spec.lua
+++ tests/parser/headers_spec.lua
@@ -6,18 +6,45 @@ describe("headers", function()
     assert.is_false(h:has("accept"))
     assert.is_nil(h["accept"])
   end)
+
   it("case insensitive get/set", function()
     local h = HTTPHeaders:new()
     h["ACCEPT"] = "text/plain"
     assert.same({ "text/plain" }, h["accept"])
   end)
-  it("has value", function()
+
+  it("has", function()
     local h = HTTPHeaders:new()
     h["ACCEPT"] = "text/plain"
     assert.is_true(h:has("Accept"))
-    assert.is_true(h:has_value("Accept", "text/plain"))
-    assert.is_false(h:has_value("Accept", "text/json"))
+    assert.is_true(h:has("accept"))
+    assert.is_false(h:has("Axxept"))
   end)
+
+  it("has value", function()
+    local h = HTTPHeaders:new()
+    h["ACCEPT"] = "text/plain"
+    assert.is_true(h:has("Accept", "text/plain"))
+    assert.is_true(h:has("accept", "text/plain"))
+    assert.is_true(h:has("accept", "Text/plain"))
+    assert.is_false(h:has("Accept", "text/json"))
+  end)
+
+  it("has exact", function()
+    local h = HTTPHeaders:new()
+    h["X-REQUEST-TYPE"] = "graphql"
+    assert.is_true(h:has_exact("X-REQUEST-TYPE"))
+    assert.is_false(h:has_exact("x-request-type"))
+  end)
+
+  it("has exact value", function()
+    local h = HTTPHeaders:new()
+    h["X-REQUEST-TYPE"] = "graphql"
+    assert.is_true(h:has_exact("X-REQUEST-TYPE", "graphql"))
+    assert.is_false(h:has_exact("x-request-type", "GRAPHQL"))
+    assert.is_false(h:has_exact("X-REQUEST-TYPE", "GRAPHQL"))
+  end)
+
   it("multiple header values", function()
     local h = HTTPHeaders:new()
     h["accept"] = "text/html"
@@ -25,6 +52,7 @@ describe("headers", function()
     h["aCCEPT"] = "*/*"
     assert.same({ "text/html", "text/plain", "*/*" }, h["Accept"])
   end)
+
   it("flat pairs", function()
     local h = HTTPHeaders:new()
     h["accept"] = "text/html"
@@ -36,8 +64,8 @@ describe("headers", function()
     end
     local expected = {
       { "accept", "text/html" },
-      { "accept", "text/plain" },
-      { "accept", "*/*" },
+      { "Accept", "text/plain" },
+      { "aCCEPT", "*/*" },
     }
     assert.same(expected, flat)
   end)
