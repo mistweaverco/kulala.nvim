f4abb3a65deed9620f5553efb7d1ff8f3b4224f1
Marco Kellershoff <marco@kellershoff.net>
2025-01-27 12:44:20 +0100

    feat(api): add `after_next_request`.
    This triggers only once for the next request and
    then flushes the queue.

--- lua/kulala/api/init.lua
+++ lua/kulala/api/init.lua
@@ -4,6 +4,7 @@ local Fs = require("kulala.utils.fs")
 local M = {}
 
 M.events = {
+  ["after_next_request"] = {},
   ["after_request"] = {},
 }
 
@@ -19,6 +20,18 @@ M.trigger = function(name)
   if M.events[name] == nil then
     Logger.error("Invalid event name: " .. name)
     return
+  end
+  if name == "after_next_request" then
+    local headers = Fs.read_file(Globals.HEADERS_FILE)
+    local body = Fs.read_file(Globals.BODY_FILE)
+    for _, callback in ipairs(M.events[name]) do
+      callback({
+        headers = headers,
+        body = body,
+      })
+    end
+    -- reset the queue
+    M.events[name] = {}
   end
   if name == "after_request" then
     local headers = Fs.read_file(Globals.HEADERS_FILE)
