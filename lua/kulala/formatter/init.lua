26ff1c80c8a3052837b010f6c81356b25d79c405
Yaro <yaro@mail.ru>
2025-04-15 14:51:15 +0100

    refact: add error message if formatter not found

--- lua/kulala/formatter/init.lua
+++ lua/kulala/formatter/init.lua
@@ -7,13 +7,17 @@ M.format = function(formatter, contents)
     return formatter(contents)
   elseif type(formatter) == "table" then
     local cmd = formatter
-    local ret = vim.system(cmd, { stdin = contents, text = true }):wait()
 
-    if ret.code == 0 then
-      return ret.stdout
-    else
-      Logger.warn(("Error running external formatter: %s"):format(ret.stderr))
+    local status, result = pcall(function()
+      return vim.system(cmd, { stdin = contents, text = true }):wait()
+    end)
+
+    if not status or result.code ~= 0 then
+      Logger.warn(("Error running external formatter: %s"):format(not status and result or result.stderr))
+      return contents
     end
+
+    return result.stdout
   end
 
   return contents
