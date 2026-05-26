adacb40946fbc5be0ed9447afa4b1e2389b68570
Marco Kellershoff <marco@mistweaverco.com>
2026-05-26 15:56:31 +0200

    fix(ci): add safe-guard for version mismatch on release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,15 @@
 set -euo pipefail
 
 GH_TAG="v$VERSION"
+PLUGIN_VERSION_FILE="./lua/kulala/globals/versions/plugin.lua"
 
-set_version() {
-  ./scripts/set-version.sh
+check_version_matches_tag() {
+  local v
+  v=$(cat "$PLUGIN_VERSION_FILE" | grep 'return "' | sed 's/return "\(.*\)"/\1/')
+  if [[ "$VERSION" != "$v" ]]; then
+    echo "Found version $v in $PLUGIN_VERSION_FILE, but expected $VERSION."
+    exit 1
+  fi
 }
 
 do_gh_release() {
@@ -14,7 +20,7 @@ do_gh_release() {
 }
 
 boot() {
-  set_version
+  check_version_matches_tag
   do_gh_release
 }
 
