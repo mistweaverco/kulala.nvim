e3465e975a9cf34bff21db97d66c407e6ab07a8a
Marco Kellershoff <marco@mistweaverco.com>
2026-05-31 17:12:49 +0200

    fix(ci): use pre-compiled binary

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -2,16 +2,11 @@
 
 set -eo pipefail
 
-sudo apt-get update && \
-  sudo apt-get install -y \
-    curl \
-    liblua5.3-dev \
-    lua5.3 \
-    luarocks
+sudo apt-get update
+sudo apt-get install -y curl liblua5.3-dev lua5.3 luarocks
 
 sudo luarocks install luacheck
 
-curl --proto '=https' --tlsv1.2 -sSf https://rustup.rs | sh
-cargo install stylua --features luajit --features lua52 --features lua53 --features lua54
+sudo npm install -g @johnnymorganz/stylua-bin
 
 sudo snap install vale
