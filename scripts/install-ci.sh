b908b0c7a1fbe56b6ec5af75d6f30abd8a57034b
Marco Kellershoff <marco@mistweaverco.com>
2026-05-31 17:21:33 +0200

    fix(ci): install neovim from github releases

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -2,8 +2,16 @@
 
 set -eo pipefail
 
+NEOVIM_VERSION=${NEOVIM_VERSION:-"0.12.2"}
+
 sudo apt-get update
-sudo apt-get install -y curl liblua5.3-dev lua5.3 luarocks neovim
+sudo apt-get install -y curl liblua5.3-dev lua5.3 luarocks
+
+# Install Neovim
+curl -LO "https://github.com/neovim/neovim/releases/download/v$NEOVIM_VERSION/nvim-linux-x86_64.tar.gz"
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo "/opt/nvim-linux-x86_64/bin" >> "$GITHUB_PATH"
 
 sudo luarocks install luacheck
 
