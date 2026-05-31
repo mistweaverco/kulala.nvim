a79bf8892f04914b5d27cef44a1663d3b8a1856c
Marco Kellershoff <marco@mistweaverco.com>
2026-05-31 17:05:49 +0200

    fix(ci): install-ci.sh

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -3,9 +3,12 @@
 set -eo pipefail
 
 sudo apt update && \
-  sudo apt install -y \
-    curl \
-    luacheck \
-    neovim \
-    stylua \
-    vale
+  sudo apt install lua5.3 luarocks curl -y
+
+
+sudo luarocks install luacheck
+
+curl --proto '=https' --tlsv1.2 -sSf https://rustup.rs | sh
+cargo install stylua --features luajit --features lua52 --features lua53 --features lua54
+
+sudo snap install vale
